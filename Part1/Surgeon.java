fea71850f6427f0682aaa036932cac5b67ae135d
Vidhi Thacker <vidhithacker@Vidhis-MacBook-Air.local>
2023-03-25 23:43:54 -0400

    initial commit

---
+++ Part1/Surgeon.java
@@ -0,0 +1,29 @@
+public class Surgeon extends Doctor {
+    private boolean operating;
+
+    public Surgeon(String name, int number, String specialty, boolean operating) {
+        super(name, number, specialty);
+        this.operating = operating;
+    }
+
+    public boolean isOperating() {
+        return operating;
+    }
+
+    public void setOperating(boolean operating) {
+        this.operating = operating;
+    }
+
+    @Override
+    public void work() {
+        super.work();
+        if (operating) {
+            System.out.println(name + " is operating now.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Operating: " + operating;
+    }
+}
